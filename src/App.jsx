577e0285c6038e7eb7898e5d59eac447fe2d0b9d
Sairkko <bastien.98@orange.fr>
2024-10-08 15:25:54 +0200

    feat: 4nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { DarkModeProvider } from './contexts/DarkModeContext';
 import Navbar from './pages/Navbar';
 import Home from './pages/Home';
-import {UserProvider} from "./contexts/UserContext.jsx";
 
 function App() {
     return (
         <div>
-                <Navbar />
-                <Home />
+            <Navbar />
+            <Home />
         </div>
     );
 }
