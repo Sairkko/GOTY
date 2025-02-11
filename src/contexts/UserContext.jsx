1b07ff64eacc1bbdda86e1e0084ad37685f90254
Sairkko <bastien.98@orange.fr>
2025-02-11 17:18:06 +0100

    feat: jeu back

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -17,7 +17,7 @@ export const UserProvider = ({ children }) => {
         const { firstname, email, lastname, username, password } = values;
 
         try {
-            const response = await axios.post(`https://jdr-lotr-back.onrender.com/register`, {
+            const response = await axios.post(`/register`, {
                 firstname,
                 email,
                 lastname,
@@ -45,7 +45,7 @@ export const UserProvider = ({ children }) => {
         const { email, password } = values;
 
         try {
-            const response = await axios.post(`${API_URL}/login`, {
+            const response = await axios.post(`/login`, {
                 email,
                 password
             });
