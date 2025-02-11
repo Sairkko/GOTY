8e74a706eef33e3522e3704896bdc7a008ad7b68
Sairkko <bastien.98@orange.fr>
2025-02-11 12:35:44 +0100

    feat: jeu front

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -17,7 +17,7 @@ export const UserProvider = ({ children }) => {
         const { firstname, email, lastname, username, password } = values;
 
         try {
-            const response = await axios.post(`https://goty-phozqwwan-sairkkos-projects.vercel.app/register`, {
+            const response = await axios.post(`http://0.0.0.0:10000/register`, {
                 firstname,
                 email,
                 lastname,
